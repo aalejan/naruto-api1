c69fae1895b8b4dd5483b0567a40a493ef96f9c2
Alex Alejandro <eaglesalejandro.aa@gmail.com>
2020-12-13 17:27:43 -0500

    controllers, migrations, and models folder created

--- naruto.sql
+++ naruto.sql
@@ -5,9 +5,53 @@ USE naruto;
 CREATE TABLE narutoCharacters (
     id INT auto_increment,
     name VARCHAR(255),
-    village VARCHAR(255),
-    powerstats VARCHAR (255),
-    techniques VARCHAR(255),
+    createdAt DATETIME DEFAULT NOW(),
+    updatedAt DATETIME DEFAULT NOW() ON UPDATE NOW(),
+    deletedAt DATETIME,
+    PRIMARY KEY(id)
+);
+
+CREATE TABLE characters(
+    id INT auto_increment,
+    name VARCHAR(255),
+    createdAt DATETIME DEFAULT NOW(),
+    updatedAt DATETIME DEFAULT NOW() ON UPDATE NOW(),
+    PRIMARY KEY(id)
+)
+
+CREATE TABLE charactersVillages(
+    characterId INT,
+    villageId INT,
+    createdAt DATETIME DEFAULT NOW(),
+    updatedAt DATETIME DEFAULT NOW() ON UPDATE NOW(),
+    PRIMARY KEY(characterId, villageId),
+    FOREIGN KEY(characterId) REFERENCES characters(id),
+    FOREIGN KEY(villageId) REFERENCES villages(id)
+)
+
+CREATE TABLE villages(
+    id INT auto_increment,
+    name VARCHAR(255),
+    characterId INT,
+    createdAt DATETIME DEFAULT NOW(),
+    updatedAt DATETIME DEFAULT NOW() ON UPDATE NOW(),
+    PRIMARY KEY(id),
+    FOREIGN KEY(characterId) REFERENCES characters(id)
+)
+
+CREATE TABLE techniques(
+     id INT auto_increment,
+    name VARCHAR(255),
+    characterId INT,
+    createdAt DATETIME DEFAULT NOW(),
+    updatedAt DATETIME DEFAULT NOW() ON UPDATE NOW(),
+    PRIMARY KEY(id),
+    FOREIGN KEY(characterId) REFERENCES characters(id)
+)
+
+CREATE TABLE attackPotency(
+    id INT auto_increment,
+    name VARCHAR(255),
     createdAt DATETIME DEFAULT NOW(),
     updatedAt DATETIME DEFAULT NOW() ON UPDATE NOW(),
     deletedAt DATETIME,
