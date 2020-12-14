b664a7af9cc06122624dfc86b5e743d85717785c
Alex Alejandro <eaglesalejandro.aa@gmail.com>
2020-12-14 14:12:07 -0500

    villages model created

---
+++ models/villages.js
@@ -0,0 +1,11 @@
+const Characters = require("./characters")
+
+const Villages= (connection , Sequelize) => {
+    return connection.define('villages', {
+        id: {type : Sequelize.INTEGER, auto_increment: true, primaryKey: true },
+        name: { type: Sequelize.STRING },
+        characterId: { type: Sequelize.INTEGER, references: { model: Characters, key: 'id'}}
+    }, { paranoid: true })
+}
+
+module.exports = Villages
