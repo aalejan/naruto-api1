87ac5ec4df64c07d40d1e85622c0e5c408a4997e
Alex Alejandro <eaglesalejandro.aa@gmail.com>
2020-12-18 12:55:22 -0500

    bulkInsert made into villages table

---
+++ migrations/20201218175200-villages-bulkInsert-creation.js
@@ -0,0 +1,52 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    /**
+     * Add altering commands here.
+     *
+     * Example:
+     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
+     */
+    return queryInterface.bulkInsert('villages', [
+      {
+        id: 1,
+        name: 'Konahagakure'
+      },
+      {
+        id: 2,
+        name: 'Kirigakure'
+      },
+      {
+        id: 3,
+        name: 'Kumogakure'
+      },
+      {
+        id: 4,
+        name: 'Iwagakure'
+      },
+      {
+        id: 5,
+        name: 'Amegakure'
+      },
+      {
+        id: 6,
+        name: 'Otogakure'
+      },
+      {
+        id: 7,
+        name: 'Sunagakure'
+      },
+    ])
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    /**
+     * Add reverting commands here.
+     *
+     * Example:
+     * await queryInterface.dropTable('users');
+     */
+    return queryInterface.bulkDelete('villages')
+  }
+};
