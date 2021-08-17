62f91707927c0a542a9f4764277d0d16693fc364
Alex Alejandro <eaglesalejandro.aa@gmail.com>
2021-08-17 19:40:08 -0400

    pushing DB to heroku

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ app.use(express.static('public'))
 
 app.set('view engine', 'pug')
 
-app.listen(8600, () => console.log('Listening on port 8600...'))
+app.listen(process.env.PORT || 8600, () => console.log('Listening on port 8600...'))
 
 app.get('/', (req, res) =>{
     res.render('index')
